954e594adaaff4456b6e0d401c55a0ddded9adcd
Radomir <RVTarasov@1cbit.ru>
2021-09-18 18:29:56 +0300

    Небольшие правки

--- bottle_test1.py
+++ bottle_test1.py
@@ -1,26 +1,35 @@
+import sentry_sdk
 import os
 
-from bottle import route, run
+from bottle import Bottle, request
+from sentry_sdk.integrations.bottle import BottleIntegration
+
+sentry_sdk.init(
+    dsn="https://bfc844c58c804666a4873c2dfe157ace@o1005073.ingest.sentry.io/5965960",
+    integrations=[BottleIntegration()]
+)
+
+app = Bottle()
 
 
-@route('/')
+@app.route('/')
 def index():
     raise RuntimeError("There is an error!")
     return
 
 
-@route('/success')
+@app.route('/success')
 def index():
     return
 
 
-@route('/fail')
+@app.route('/fail')
 def index():
     raise RuntimeError("There is an error!")
     return
 
 
 if os.environ.get('APP_LOCATION') == 'heroku':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
-    run(host='localhost', port=8080, debug=True)
+    app.run(host='localhost', port=8080, debug=True)
